21765340c99c8a4a673991c9286048c59a82fd20
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-12-05 00:38:08 +0530

    Added day4 JavScript solution

---
+++ day4/p1.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const fileContents = fs.readFileSync("./input.txt", "utf8", "r").split(/\r?\n/);
+
+const contentsArr = fileContents.map((pair) => {
+  return pair.split(/-|,/g);
+});
+
+let overlappingPairs = 0;
+contentsArr.forEach((pair) => {
+  pair.forEach((str, i, arr) => {
+    arr[i] = parseInt(str);
+  });
+
+  if (pair[2] >= pair[0] && pair[3] <= pair[1]) {
+    overlappingPairs++;
+  } else if (pair[0] >= pair[2] && pair[1] <= pair[3]) {
+    overlappingPairs++;
+  }
+});
+
+console.log(overlappingPairs);
